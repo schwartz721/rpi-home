995a3773e1b905092c3a40a06146ddbf29209ddd
Andrew Schwartz <andrew@tomis.tech>
2024-01-11 18:20:23 -0700

    Start testing with servo control

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from gpiozero import Servo
 
 app = Flask(__name__)
 app.furnace_temp = 0
@@ -14,7 +15,9 @@ def get_furnace():
 
 @app.route("/furnace/<temp>", methods=["POST"])
 def post_furnace(temp):
+    servo = Servo(25)
     app.furnace_temp = temp
+    servo.value(temp / 5 - 1)
     return f"Setting furnace to {app.furnace_temp}"
 
 if __name__ == "__main__":
