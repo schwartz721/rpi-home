96ab4215127f19d78cdb345af6569ac128f536f2
Andrew Schwartz <andrew@tomis.tech>
2024-01-11 22:20:53 -0700

    Use piGPIO as the pin driver

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask
 from gpiozero import Servo
 
 app = Flask(__name__)
-app.furnace_temp = 0
+app.servo = Servo(25)
 
 # Routes
 @app.route("/")
@@ -11,19 +11,18 @@ def ping():
 
 @app.route("/furnace")
 def get_furnace():
-    return f"Furnace is set to {app.furnace_temp}"
+    # Convert from servo position (from -1 and 1) to furnace setting (from 0 and 10)
+    temp = (app.servo.value + 1) * 5
+    # Return the current furnace setting
+    return f"Furnace is set to {temp}"
 
 @app.route("/furnace/<temp>", methods=["POST"])
 def post_furnace(temp):
     try:
-        servo = Servo(25)
+        # Convert from furnace setting (from 0 to 10) to servo position (from -1 to 1)
         temp = int(temp)
-        app.furnace_temp = temp
-        pwm = temp / 5 - 1
-        pwm = max(-1, pwm)
-        pwm = min(1, pwm)
-        servo.value(pwm)
-        return f"Setting furnace to {temp}"
+        app.servo.value = min(1, max(-1, temp / 5 - 1))
+        return f"Setting furnace to {(app.servo.value + 1) * 5}"
     except Exception as e:
         return str(e)
 
