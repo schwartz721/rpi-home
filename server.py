b7f37e8a582ca6327157639f738a4ec88f155f12
Andrew Schwartz <andrew@tomis.tech>
2024-01-11 21:51:47 -0700

    Add error handling and logging to post_furnace route

--- server.py
+++ server.py
@@ -15,14 +15,17 @@ def get_furnace():
 
 @app.route("/furnace/<temp>", methods=["POST"])
 def post_furnace(temp):
-    servo = Servo(25)
-    temp = int(temp)
-    app.furnace_temp = temp
-    pwm = temp / 5 - 1
-    pwm = max(-1, pwm)
-    pwm = min(1, pwm)
-    servo.value(pwm)
-    return f"Setting furnace to {temp}"
+    try:
+        servo = Servo(25)
+        temp = int(temp)
+        app.furnace_temp = temp
+        pwm = temp / 5 - 1
+        pwm = max(-1, pwm)
+        pwm = min(1, pwm)
+        servo.value(pwm)
+        return f"Setting furnace to {temp}"
+    except Exception as e:
+        return str(e)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
