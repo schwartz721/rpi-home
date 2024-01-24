1d51bd066116597426ae44ba180a9cac7b8aad98
Andrew Schwartz <andrew@tomis.tech>
2024-01-23 20:11:17 -0700

    Change furnace control to 0 - 100 and reverse correlation with PWM

--- server.py
+++ server.py
@@ -1,8 +1,15 @@
 from flask import Flask
 import pigpio
+from time import sleep
 
 app = Flask(__name__)
 app.servo_pin = 25
+
+def percent_to_pwm(percent):
+    return max(700, min(2500, (percent - 100) * -18 + 700))
+
+def pwm_to_percent(pwm):
+    return round((pwm - 700) / -18 + 100)
 
 # Routes
 @app.route("/")
@@ -13,19 +20,21 @@ def ping():
 def get_furnace():
     servo = pigpio.pi()
     pwm = servo.get_servo_pulsewidth(app.servo_pin)
-    # Convert from servo position (from 700 to 2500) to furnace setting (from 0 to 10)
-    furnace = round((pwm - 700) / 180)
+    # Convert from servo position (from 2500 to 700) to furnace setting (from 0 to 100)
+    furnace = pwm_to_percent(pwm)
     return f"Furnace is set to {furnace}"
 
 @app.route("/furnace/<furnace>", methods=["POST"])
 def post_furnace(furnace):
     try:
         furnace = int(furnace)
-        # Convert from furnace setting (from 0 to 10) to servo position (from 700 to 2500)
+        # Convert from furnace setting (from 0 to 100) to servo position (from 2500 to 700)
         pwm = max(700, min(2500, furnace * 180 + 700))
         servo = pigpio.pi()
         servo.set_servo_pulsewidth(app.servo_pin, pwm)
-        return f"Setting furnace to {round((pwm - 700) / 180)}"
+        sleep(1)
+        servo.set_servo_pulsewidth(app.servo_pin, None)
+        return f"Setting furnace to {pwm_to_percent(pwm)}"
     except Exception as e:
         return str(e)
 
